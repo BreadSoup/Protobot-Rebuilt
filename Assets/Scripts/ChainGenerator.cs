28a0a278702148fefd56d2d19357aab47764a154
Logan <lcrane644@gmail.com>
2024-07-27 17:39:38 -0600

    added new chain code, removed old prototypes
    i tried making the new code for the chain generator, i gave up. saving this if anyone wants to work on it. The code is suppose to create a empty gameobject, then becomes a parent of any selected sprockets.

--- Assets/Scripts/ChainGenerator.cs
+++ Assets/Scripts/ChainGenerator.cs
@@ -1,32 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChainGenerator : MonoBehaviour
 {
-    public Transform line; // Assign the "Line" object in the Inspector
-    public GameObject chainLinkPrefab; // Assign the "ChainLink" object in the Inspector
-    public float linkSpacing = 0.1f; // Set the distance between each chain link
 
-    private void Start()
+    public Toggle toggle;
+
+    void Start()
     {
-        GenerateChain();
+        if (toggle != null)
+        {
+            toggle.onValueChanged.AddListener(OnToggleChanged);
+        }
     }
 
-    private void GenerateChain()
+    void OnToggleChanged(bool isOn)
     {
-        // Calculate the line's start and end points
-        Vector3 lineStart = line.position - line.localScale.z * 0.5f * Vector3.forward;
-        Vector3 lineEnd = line.position + line.localScale.z * 0.5f * Vector3.forward;
-
-        // Calculate the line's length
-        float lineLength = Vector3.Distance(lineStart, lineEnd);
-
-        // Generate the chain links along the line
-        Vector3 currentPosition = lineStart;
-        while (currentPosition.z < lineEnd.z)
+        if (isOn)
         {
-            GameObject chainLink = Instantiate(chainLinkPrefab, currentPosition, Quaternion.Euler(0, 90, 0));
-            chainLink.transform.parent = transform;
-            currentPosition += linkSpacing * Vector3.forward;
+            new GameObject("ChainGeneratorObject");
         }
     }
 }
